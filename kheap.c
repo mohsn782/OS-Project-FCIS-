0bba8e84bed6a220d8244fe279154bf812dd7e6a
Muhammed Mohsen Muhammed <mohsenmohamed782@gmail.com>
2017-04-19 18:06:35 +0200

    Update kheap.c

--- kheap.c
+++ kheap.c
@@ -7,75 +7,91 @@
 //=================================================================================//
 //============================ REQUIRED FUNCTION ==================================//
 //=================================================================================//
-struct kstruct {
-	 uint32 va;
-	 int size;
+
+struct Address_Info
+{
+	uint32 size;
+	uint32 VAddess;
 };
 
-struct ktruct arr[10];
+struct Address_Info AddInfo[100];
+int ctr=-1;
+uint32 *firstFreeVAInKHeap  = (uint32*)KERNEL_HEAP_START;
 
 void* kmalloc(unsigned int size)
 {
-	//TODO: [PROJECT 2017 - [1] Kernel Heap] kmalloc()
-	// Write your code here, remove the panic and write your code
-	panic("kmalloc() is not implemented yet...!!");
-
-	//NOTE: Allocation is continuous increasing virtual address
-	//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
-	//refer to the project presentation and documentation for details
-
-
-
-	//TODO: [PROJECT 2017 - BONUS1] Implement a Kernel allocation strategy
-	// Instead of the continuous allocation/deallocation, implement both
-	// FIRST FIT and NEXT FIT strategies
-	// use "isKHeapPlacementStrategyFIRSTFIT() ..." functions to check the current strategy
-
-
-
-	//change this "return" according to your answer
-	return NULL;
-
-
+	size = ROUNDUP(size, PAGE_SIZE);
+	int i,pages=size/PAGE_SIZE;
+	if(size <= KERNEL_HEAP_MAX - (uint32)firstFreeVAInKHeap)
+	{
+		uint32 *va = firstFreeVAInKHeap;
+		ctr++;
+		AddInfo[ctr].size = pages;
+	    AddInfo[ctr].VAddess = (uint32)va;
+		for(i=0;i<pages;i++)
+		{
+			struct Frame_Info * ptr_frame_info;
+			uint32 *ptr = (uint32 *)(firstFreeVAInKHeap);
+			allocate_frame(&ptr_frame_info);
+			map_frame(ptr_page_directory, ptr_frame_info, (void*)ptr, PERM_WRITEABLE);
+			firstFreeVAInKHeap = (uint32*) ((int)firstFreeVAInKHeap  + PAGE_SIZE);
+		}
+		return (void*)va;
+	}
+	else
+		return NULL;
 }
-
 
 void kfree(void* virtual_address)
 {
-	for (int i=0;i<10;i+=PAGE_SIZE)
+	int i,sz;
+	for(i=0;i<=ctr;i++)
 	{
-		unmap_frame(ptr_page_directory, (void*)virtual_address+i);
-
+		if(AddInfo[i].VAddess==(uint32)virtual_address)
+		{
+			sz = AddInfo[i].size;
+			break;
+		}
 	}
-
+	for (i=0;i<sz;i++)
+	{
+		unmap_frame(ptr_page_directory, (void*)virtual_address);
+		virtual_address += PAGE_SIZE;
+	}
+	tlbflush();
 }
 
 unsigned int kheap_virtual_address(unsigned int physical_address)
 {
-	//TODO: [PROJECT 2017 - [1] Kernel Heap] kheap_virtual_address()
-	// Write your code here, remove the panic and write your code
-	panic("kheap_virtual_address() is not implemented yet...!!");
-
-	//return the virtual address corresponding to given physical_address
-	//refer to the project presentation and documentation for details
-
-	//change this "return" according to your answer
+	int va = KERNEL_HEAP_START;
+	uint32 *ptr_page_table;
+	for (;va< KERNEL_HEAP_MAX; va += PAGE_SIZE)
+	{
+		if (get_frame_info(ptr_page_directory, (void *)va, &ptr_page_table) ==
+				to_frame_info(physical_address))
+			return va;
+	}
 	return 0;
 }
 
 unsigned int kheap_physical_address(unsigned int virtual_address)
 {
-	//TODO: [PROJECT 2017 - [1] Kernel Heap] kheap_physical_address()
-	// Write your code here, remove the panic and write your code
-	panic("kheap_physical_address() is not implemented yet...!!");
-
-	//return the physical address corresponding to given virtual_address
-	//refer to the project presentation and documentation for details
-
-	//change this "return" according to your answer
+	uint32 ppt,va = virtual_address;
+	uint32 *ptr_page_table = NULL;
+	get_page_table(ptr_page_directory, (void*)virtual_address, &ptr_page_table);
+	if(ptr_page_table!=NULL)
+	{
+		uint32 used = ptr_page_table[PTX(virtual_address)] & PERM_PRESENT;
+		if(used==1)
+		{
+			ppt=((ptr_page_table[PTX(virtual_address)])>>12)*4096;
+			va = va & (uint32)0x00000FFF;
+			return ppt+va;
+		}
+		return 0;
+	}
 	return 0;
 }
-
 
 //=================================================================================//
 //============================== BONUS FUNCTION ===================================//
